6809fc4e0877d3236e4bc60f0251fdf3c8e9ebe9
Marcio cley <marciocleydev@gmail.com>
2025-08-07 19:25:18 -0300

    User insert

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -5,7 +5,9 @@ import com.educandoweb.course.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,5 +25,11 @@ public class UserResource {
     public ResponseEntity<User> findById(@PathVariable Long id){//@pathVariable indica que esse (Long id) vai vim pela requisição http que está logo a cima.
         User user = service.findById(id);
         return ResponseEntity.ok().body(user); //.ok -> indica que ocorreu tudo bem e no body vai o resultado gerado pela requisiçao
+    }
+    @PostMapping
+    public ResponseEntity<User>insert(@RequestBody User user){ //@requestBody informa que o User vai vim via json e será descerializado e transformado em objeto User
+        User user1 = service.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user1.getId()).toUri();// formula para criar o caminho que será exibido na url para acesar esse objeto criado.
+        return ResponseEntity.created(uri).body(user1);//o metodo created espera um URI que é o caminho até esse objeto que foi inserido.
     }
 }
